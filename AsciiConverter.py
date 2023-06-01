721074cb361bef264c31de88f1b63b0b54f0164a
AdamHeaney <94878093+Machooey@users.noreply.github.com>
2023-05-31 22:19:20 -0400

    Created the ascii converter

--- AsciiConverter.py
+++ AsciiConverter.py
@@ -1,7 +1,56 @@
-ascii_conversion = ["$", "@", "B", "%", "8", "&", 'W', 'M', '#', '*',
-                    'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 
-                    'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X',
-                    'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', 
-                    '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', 
-                    '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 
-                    'I', ';', ':', ',', '\"', '^', '\`', '\'', '.', ' ']
+import cv2
+import tkinter as tk
+from tkinter import font
+from ctypes import windll
+
+
+windll.shcore.SetProcessDpiAwareness(1)
+
+# Create a window
+window = tk.Tk()
+
+# Set the window title
+window.title("My Window")
+
+# Set the window size
+window.geometry("1280x720")
+window.configure(bg="black")
+
+# 70 chars
+ascii_conversion = ['@', '%', '#', '*', '+', '=', '-', ':', '.']
+
+def clamp(value, min_value, max_value):
+        return max(min(value, max_value), min_value)
+
+capture = cv2.VideoCapture(0)
+custom_font = font.Font(family="Cascadia Mono", size=12)
+label = tk.Label(window, text="", font=custom_font, fg="white", bg="black")
+while True:
+    # Capture frame-by-frame
+    ret, frame = capture.read()
+
+    # If frame is read correctly, ret will be True
+    if not ret:
+        print("Failed to capture frame")
+        break
+
+    height, width, channels = frame.shape
+    ratio = height / width / 2.7
+    newWidth = 100
+    newHeight = int(ratio * newWidth)
+    frame = cv2.resize(frame, (newWidth, newHeight), cv2.INTER_CUBIC)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    asciiFrame = ''
+    for y in range(0, newHeight):
+        for x in range(0, newWidth):
+            luminance = clamp(int(frame[y, x] / 24.875), 0, 8)   # noqa: E501
+            asciiFrame = asciiFrame + ascii_conversion[luminance]  
+        asciiFrame = asciiFrame + "\n"
+    label.config(text = asciiFrame)
+
+    # Pack the label widget to display it on the window
+    label.pack()
+    window.update()
+    # print("\n" + "\n" + asciiFrame)
+window.mainloop()
+    
